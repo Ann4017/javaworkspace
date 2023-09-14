50077b60dfee269b9e653616450653a243ed30d6
ann4017 <abh4017@naver.com>
2023-09-15 04:26:11 +0900

    Update StudentManager.java

--- javaOOP4Project/src/StudentManager.java
+++ javaOOP4Project/src/StudentManager.java
@@ -10,8 +10,11 @@ public class StudentManager {
 
 
   private void read() {
+
     System.out.println("학생 이름,학과,학번,학점평균 입력하세요.");
-    while (true) {
+    int count = 0;
+
+    while (count < 4) {
       System.out.print(">> ");
       String text = scanner.nextLine();
       StringTokenizer st = new StringTokenizer(text, ",");
@@ -20,13 +23,11 @@ public class StudentManager {
         Student stu = new Student(st.nextToken(), st.nextToken(), st.nextToken(),
             Double.parseDouble(st.nextToken()));
         dept.add(stu);
+        count++;
       } else {
         System.out.println("양식에 맞게 입력해주세요.");
       }
 
-      if (dept.size() == 4) {
-        break;
-      }
       // ArrayList에 저장
     }
   }
@@ -35,9 +36,9 @@ public class StudentManager {
     Iterator<Student> it = dept.iterator();
     while (it.hasNext()) {
       Student stu = it.next();
-      System.out.println(stu.getName() + stu.getDepartment() + stu.getId() + stu.getGrade())1;
-      System.out.printf("%s %s %s %d\n", stu.getName(), stu.getDepartment(), stu.getId(),
-          stu.getGrade());
+
+      System.out.printf("이름: %s, 학과: %s, 학번: %s, 학점평균: %.1f\n", stu.getName(), stu.getDepartment(),
+          stu.getId(), stu.getGrade());
       // 학생 정보 출력
     }
   }
@@ -46,24 +47,37 @@ public class StudentManager {
     while (true) {
       System.out.print("학생 이름 >> ");
       String name = scanner.nextLine(); // 학생 이름 입력
+      boolean found = false;
 
       if (name.equals("그만")) {
         return; // 종료
       }
 
-      for (int i = 0; i < dept.size(); i++) { // dept에 있는 모든 학생 검색
-        if (name.equals(dept.get(i))) {
-          System.out.println(dept.get(i));
-        }
 
+
+      for (int i = 0; i < dept.size(); i++) { // dept에 있는 모든 학생 검색
+
+        Student stu = dept.get(i);
+
+        if (name.equals(stu.getName())) {
+          System.out.printf("이름: %s, 학과: %s, 학번: %s, 학점평균: %.1f\n", stu.getName(),
+              stu.getDepartment(), stu.getId(), stu.getGrade());
+          found = true;
+          break;
+        }
       } // end of while
+
+      if (!found) {
+        System.out.println("찾을 수 없는 학생 정보 입니다.");
+      }
     }
   }
 
   public void run() {
     read();
     printAll();
-    // processQuery();
+    processQuery();
+    scanner.close();
   }
 
 
